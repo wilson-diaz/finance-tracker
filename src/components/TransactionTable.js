402578b33fd9eb8e12cb8f5ee38cbb59d4e5d0bc
Wilson Diaz <wdfiguereo@gmail.com>
2022-01-12 23:57:47 -0500

    implement simple react-table for transactions

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -1,27 +1,86 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table'
 import { Table } from 'semantic-ui-react'
-import TransactionItem from './TransactionItem'
 
 const TransactionTable = () => {
-  const transactions = [
-    { id: 1, date: '12-6-2021', type: 'WITHDRAW', allocation: 'Food', amount: 50.87, reason: 'went grocery shopping' },
-    { id: 2, date: '12-6-2021', type: 'WITHDRAW', allocation: 'Investments', amount: 200.00, reason: 'bought some stock' },
-    { id: 3, date: '12-7-2021', type: 'DEPOSIT', allocation: 'Savings', amount: 20.00, reason: 'found $20 bill' },
-  ]
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Date',
+        accessor: 'date'
+      },
+      {
+        Header: 'Category',
+        accessor: 'category'
+      },
+      {
+        Header: 'Amount ($)',
+        accessor: 'amount'
+      },
+      {
+        Header: 'Details',
+        accessor: 'details'
+      }
+    ],
+    []
+  )
+  const data = useMemo(
+    () => [
+      { date: '12-6-2021', category: 'Food', amount: 50.87, details: 'went grocery shopping' },
+      { date: '12-6-2021', category: 'Investments', amount: 200.00, details: 'bought some stock' },
+      { date: '12-7-2021', category: 'Savings', amount: 20.00, details: 'found $20 bill' },
+    ],
+    []
+  )
+
+  const myTable = useTable({ columns, data })
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = myTable
+
+  console.log(headerGroups, rows)
 
   return (
-    <Table celled>
+    <Table celled {...getTableProps()}>
       <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Date</Table.HeaderCell>
-          <Table.HeaderCell>Type</Table.HeaderCell>
-          <Table.HeaderCell>Allocation</Table.HeaderCell>
-          <Table.HeaderCell>Amount</Table.HeaderCell>
-          <Table.HeaderCell>Reason</Table.HeaderCell>
-        </Table.Row>
+        {
+          headerGroups.map(headerGroup => (
+            <Table.Row key={0} {...headerGroup.getHeaderGroupProps()}>
+              {
+                headerGroup.headers.map(column => (
+                  <Table.HeaderCell key={column.id}  {...column.getHeaderProps()}>
+                    {column.render('Header')}
+                  </Table.HeaderCell>
+                ))
+              }
+            </Table.Row>
+          ))
+        }
       </Table.Header>
-      <Table.Body>
-        {transactions.map(t => <TransactionItem key={t.id} transaction={t} />)}
+      <Table.Body {...getTableBodyProps()}>
+        {
+          rows.map(row => {
+            prepareRow(row)
+            return (
+              <Table.Row  key={row.id} {...row.getRowProps()}>
+                {
+                  row.cells.map(cell => {
+                    return (
+                      <Table.Cell key={cell.column.id} {...cell.getCellProps()}>
+                        {cell.render('Cell')}
+                      </Table.Cell>
+                    )
+                  })
+                }
+              </Table.Row>
+            )
+          })
+        }
       </Table.Body>
     </Table>
   )
