0f15d2d88bbe8c00a0753b266e83a198d7007e5e
Wilson Diaz <wdfiguereo@gmail.com>
2022-01-17 21:19:53 -0500

    add apollo client

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import 'semantic-ui-css/semantic.min.css'
 
+import {
+  ApolloClient, ApolloProvider, HttpLink, InMemoryCache
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root')
 )
