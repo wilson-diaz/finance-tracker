2e33797da61ac77655136a280f53a84cae3baa00
Wilson Diaz <wdfiguereo@gmail.com>
2022-01-19 09:45:40 -0500

    fix max date func

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -3,8 +3,9 @@ import { Form } from 'semantic-ui-react'
 
 const getMaxDate = () => {
   const today = new Date()
-  const dayString = today.getDay() < 10 ? '0' + today.getDate() : today.getDay()
-  return `${today.getFullYear()}-${today.getMonth() + 1}-${dayString}`
+  const dayString = today.getDate() < 10 ? '0' + today.getDate() : today.getDate()
+  const monthString = today.getMonth() + 1 < 10 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1
+  return `${today.getFullYear()}-${monthString}-${dayString}`
 }
 
 const TransactionForm = () => (
