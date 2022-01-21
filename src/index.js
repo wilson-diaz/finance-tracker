38ece08341a678b2f5e4bf16ed50684803fe7baf
Wilson Diaz <wdfiguereo@gmail.com>
2022-01-20 23:28:14 -0500

    add auth header to all requests

--- src/index.js
+++ src/index.js
@@ -5,14 +5,25 @@ import App from './App'
 import 'semantic-ui-css/semantic.min.css'
 
 import {
-  ApolloClient, ApolloProvider, HttpLink, InMemoryCache
+  ApolloClient, ApolloProvider, HttpLink, InMemoryCache, from
 } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('userToken')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null
+    }
+  }
+})
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-  })
+  link: from([authLink, httpLink])
 })
 
 ReactDOM.render(
