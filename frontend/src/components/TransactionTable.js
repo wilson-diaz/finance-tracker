7fd38a748fd2fedf19712566ffbd7d4d6b19e5c2
Wilson Diaz <wdfiguereo@gmail.com>
2022-02-02 08:31:02 -0500

    add column filters in footer

--- frontend/src/components/TransactionTable.js
+++ frontend/src/components/TransactionTable.js
@@ -1,8 +1,114 @@
+/* eslint-disable react/prop-types */
 import { useQuery } from '@apollo/client'
 import React, { useMemo } from 'react'
 import { useFilters, usePagination, useSortBy, useTable } from 'react-table'
-import { Table, Icon, Button, Select, Grid } from 'semantic-ui-react'
+import { Table, Icon, Input, Button, Select, Grid } from 'semantic-ui-react'
 import { GET_USER_TRANSACTIONS } from '../queries'
+
+const TextFilter = ({ column: { filterValue, setFilter } }) => {
+  return (
+    <Input
+      type='text'
+      value={filterValue || ''}
+      onChange={e => setFilter(e.target.value || undefined)}
+      placeholder='Search...'
+    />
+  )
+}
+
+// dropdown filter with options based on unique values in column
+const SelectFilter = ({ column: { filterValue, setFilter, preFilteredRows, id } }) => {
+  const selectOptions = useMemo(() => {
+    const options = new Set()
+    preFilteredRows.forEach((row) => {
+      options.add(row.values[id])
+    })
+    return [...options.values()].map((val, i) => ({ key: i, value: val, text: val }))
+  }, [id, preFilteredRows])
+
+  return (
+    <Select
+      value={filterValue}
+      placeholder='Category'
+      onChange={(e, { value }) => setFilter(value || undefined)}
+      options={[
+        { key: '-1', value: '', text: 'All' },
+        ...selectOptions
+      ]}
+    />
+  )
+}
+
+const createRangeFilter = inputType  => {
+  const castFilterVal = val => {
+    if (inputType === 'number') {
+      return Number(val)
+    } else if (inputType === 'date') {
+      return Date.parse(val)
+    }
+  }
+
+  // used to populate input prop with valid value to display
+  const getInputValue = val => {
+    if (inputType === 'date') {
+      const date = new Date(val)
+      const dayString = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+      const monthString = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+      return `${date.getFullYear()}-${monthString}-${dayString}`
+    }
+
+    return val
+  }
+
+  // the 'between' filter filters based on two values set in the filterValue array
+  return function RangeFilter({ column: { filterValue = [], setFilter } }) {
+    return (
+      <div>
+        <Input
+          type={inputType}
+          value={getInputValue(filterValue[0]) || ''}
+          placeholder='Min'
+          onChange={e => {
+            setFilter((old = []) => [e.target.value ? castFilterVal(e.target.value) : undefined, old[1]])
+          }}
+          style={{
+            width: '135px',
+            marginRight: '0.5rem'
+          }}
+        />
+        to
+        <Input
+          type={inputType}
+          value={getInputValue(filterValue[1]) || ''}
+          placeholder='Max'
+          onChange={e => {
+            setFilter((old = []) => [old[0], e.target.value ? castFilterVal(e.target.value) : undefined])
+          }}
+          style={{
+            width: '135px',
+            marginLeft: '0.5rem'
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+// custom filter to handle dates
+const dateRangeFilterFunc = (rows, id, filterValue) => {
+  return rows.filter(row => {
+    if (filterValue[0] && filterValue[1]) {
+      return filterValue[0] <= Date.parse(row.values[id])
+        && Date.parse(row.values[id]) <= filterValue[1]
+    } else if (filterValue[0]) {
+      return filterValue[0] <= Date.parse(row.values[id])
+    } else if (filterValue[1]) {
+      return Date.parse(row.values[id]) <= filterValue[1]
+    } else {
+      return true
+    }
+  })
+}
 
 const TransactionTable = () => {
   const transactionsResult = useQuery(GET_USER_TRANSACTIONS)
@@ -30,19 +136,25 @@ const TransactionTable = () => {
     () => [
       {
         Header: 'Date',
+        Footer: createRangeFilter('date'),
+        filter: 'dateRange',
         accessor: 'date'
       },
       {
         Header: 'Amount ($)',
+        Footer: createRangeFilter('number'),
+        filter: 'between',
         accessor: 'amount'
       },
       {
         Header: 'Category',
+        Footer: SelectFilter,
         accessor: 'category',
         disableSortBy: true
       },
       {
         Header: 'Details',
+        Footer: TextFilter,
         accessor: 'details',
         disableSortBy: true
       }
@@ -56,7 +168,10 @@ const TransactionTable = () => {
       data,
       initialState: {
         sortBy: useMemo(() => [{ id: 'date', desc: true }])
-      }
+      },
+      filterTypes: useMemo(() => ({
+        dateRange: dateRangeFilterFunc
+      }))
     },
     useFilters,
     useSortBy,
@@ -67,6 +182,7 @@ const TransactionTable = () => {
     getTableProps,
     getTableBodyProps,
     headerGroups,
+    footerGroups,
     page,
     prepareRow,
     pageOptions,
@@ -79,7 +195,6 @@ const TransactionTable = () => {
     previousPage,
     pageCount
   } = myTable
-
 
   return (
     <div>
@@ -120,6 +235,19 @@ const TransactionTable = () => {
             })
           }
         </Table.Body>
+        <Table.Footer>
+          {
+            footerGroups.map(group => (
+              <Table.Row key={group.id} {...group.getFooterGroupProps()}>
+                {
+                  group.headers.map(column => (
+                    <Table.HeaderCell key={column.id} {...column.getFooterProps()}>{column.render('Footer')}</Table.HeaderCell>
+                  ))
+                }
+              </Table.Row>
+            ))
+          }
+        </Table.Footer>
       </Table>
       <Grid columns={3}>
         <Grid.Column>
