643d8a5c01a5722b746a8ef38072e5e600dcf0a0
Wilson Diaz <wdfiguereo@gmail.com>
2022-02-02 22:17:44 -0500

    update login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { useApolloClient } from '@apollo/client'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Image } from 'semantic-ui-react'
 import NavMenu from './components/NavMenu'
 import AnalyzePage from './components/AnalyzePage'
 import TransactionPage from './components/TransactionPage'
 import LoginForm from './components/LoginForm'
+import icon from './assets/favicon.ico'
 
 const App = () => {
   const [token, setToken] = useState(null)
@@ -22,24 +23,25 @@ const App = () => {
 
   if (!token) {
     return (
-      <Container>
-        <Header as="h1">Finance Tracker</Header>
+      <Container style={appStyle}>
+        <Header as="h1" icon textAlign='center'>
+          <Image src={icon} />
+          <Header.Content>Finance Tracker</Header.Content>
+        </Header>
         <LoginForm setToken={setToken} />
       </Container>
     )
   }
   return (
-    <div style={appStyle}>
-      <Container>
-        <Header as="h1">Finance Tracker</Header>
-        <NavMenu client={client} setToken={setToken} />
-        <Routes>
-          <Route exact path='/' element={<AnalyzePage />} />
-          <Route exact path='analyze' element={<AnalyzePage />} />
-          <Route exact path='transactions' element={<TransactionPage />} />
-        </Routes>
-      </Container>
-    </div>
+    <Container style={appStyle}>
+      <Header as="h1">Finance Tracker</Header>
+      <NavMenu client={client} setToken={setToken} />
+      <Routes>
+        <Route exact path='/' element={<AnalyzePage />} />
+        <Route exact path='analyze' element={<AnalyzePage />} />
+        <Route exact path='transactions' element={<TransactionPage />} />
+      </Routes>
+    </Container>
   )
 }
 
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useLazyQuery } from '@apollo/client'
 import { LOGIN } from '../queries'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Segment, Grid } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 
 const LoginForm = ({ setToken }) => {
@@ -21,19 +21,25 @@ const LoginForm = ({ setToken }) => {
   }, [result.data])
 
   return (
-    <div>
-      <Form>
-        <Form.Field>
-          <label>Username</label>
-          <input value={username} onChange={(e) => setUsername(e.target.value)} />
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-        </Form.Field>
-        <Button type='submit' onClick={handleLogin}>Login</Button>
-      </Form>
-    </div>
+    <Grid columns={3}>
+      <Grid.Column></Grid.Column>
+      <Grid.Column>
+        <Segment>
+          <Form>
+            <Form.Field width={16}>
+              <label>Username</label>
+              <input value={username} onChange={(e) => setUsername(e.target.value)} />
+            </Form.Field>
+            <Form.Field width={16}>
+              <label>Password</label>
+              <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            </Form.Field>
+            <Button type='submit' onClick={handleLogin}>Log In</Button>
+          </Form>
+        </Segment>
+      </Grid.Column>
+      <Grid.Column></Grid.Column>
+    </Grid>
   )
 }
 LoginForm.propTypes = {
