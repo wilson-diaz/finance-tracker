2ce55f6b727401b56457ba9653cf9925c706aa8a
Wilson Diaz <wdfiguereo@gmail.com>
2022-03-26 17:08:14 -0400

    add category form to categories view

--- frontend/src/components/CategoriesView.js
+++ frontend/src/components/CategoriesView.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useMutation, useQuery } from '@apollo/client'
 import { GET_USER_CATEGORIES, EDIT_CATEGORY, DELETE_CATEGORY } from '../queries'
 import { Segment, Header, Grid, Message, Item, Button, Icon } from 'semantic-ui-react'
+import CategoryForm from './CategoryForm'
 
 const CategoriesView = () => {
   const PAGE_SIZE = 5
@@ -53,6 +54,15 @@ const CategoriesView = () => {
     deleteCategory({ variables: { id: x.id } })
   }
 
+  if (!categoryResult.data || categoryResult.data.userCategories.length === 0) {
+    return (
+      <>
+        <Message header='No Categories' content='You do not have any categories yet. Add one using the form.' />
+        <CategoryForm />
+      </>
+    )
+  }
+
   return (
     <Segment style={{ paddingBottom: '3em' }}>
       <Header as='h3'>Expense Categories</Header>
@@ -65,56 +75,56 @@ const CategoriesView = () => {
               <Message.Item><b>Delete</b>: Permanently deletes the category and all of its transactions.</Message.Item>
             </Message.List>
           </Message>
+          <Header as='h3'>Add Category</Header>
+          <CategoryForm />
         </Grid.Column>
         <Grid.Column>
           {
-            !categoryResult.data || categoryResult.data.userCategories.length === 0
-              ? <Message header='No Categories' content='You do not have any categories yet. Add one using the form.' />
-              : <>
-                <Item.Group divided style={{ height: '30em' }}>
-                  {slicedCategories.map(cat => {
-                    return (
-                      <Item key={cat.id}>
-                        <Grid container columns={2}>
-                          <Grid.Row>
-                            <Grid.Column>
-                              <Item.Header as='h4'>{cat.name}</Item.Header>
-                              <Item.Meta>Number of transactions: {cat.numTransactions}</Item.Meta>
-                            </Grid.Column>
-                            <Grid.Column verticalAlign='middle'>
-                              <Item.Content>
-                                <Item.Description>
-                                  <Button size='tiny' color={cat.isEnabled ? 'yellow' : 'green'}
-                                    style={{ marginRight: '1em' }}
-                                    onClick={() => handleEdit({ id: cat.id, isEnabled: !cat.isEnabled })}
-                                  >
-                                    <Icon name={cat.isEnabled ? 'minus square' : 'plus square'} />
-                                    {cat.isEnabled ? 'Disable' : 'Enable'}
-                                  </Button>
-                                  <Button size='tiny' negative onClick={() => handleDelete(cat)}>
-                                    <Icon name='trash alternate' />
-                                    Delete
-                                  </Button>
-                                </Item.Description>
-                              </Item.Content>
-                            </Grid.Column>
-                          </Grid.Row>
-                        </Grid>
-                      </Item>
-                    )
-                  })}
-                </Item.Group><Button icon disabled={0 > pageIndex - PAGE_SIZE}
-                  onClick={() => setPageIndex(pageIndex - PAGE_SIZE)}
-                  style={{ width: '5em' }}
-                >
-                  <Icon name='angle left' />
-                </Button><Button icon disabled={categoryResult.data.userCategories.length <= pageIndex + PAGE_SIZE}
-                  onClick={() => setPageIndex(pageIndex + PAGE_SIZE)}
-                  style={{ width: '5em' }}
-                >
-                  <Icon name='angle right' />
-                </Button>
-              </>
+            <>
+              <Item.Group divided style={{ height: '30em' }}>
+                {slicedCategories.map(cat => {
+                  return (
+                    <Item key={cat.id}>
+                      <Grid container columns={2}>
+                        <Grid.Row>
+                          <Grid.Column>
+                            <Item.Header as='h4'>{cat.name}</Item.Header>
+                            <Item.Meta>Number of transactions: {cat.numTransactions}</Item.Meta>
+                          </Grid.Column>
+                          <Grid.Column verticalAlign='middle'>
+                            <Item.Content>
+                              <Item.Description>
+                                <Button size='tiny' color={cat.isEnabled ? 'yellow' : 'green'}
+                                  style={{ marginRight: '1em' }}
+                                  onClick={() => handleEdit({ id: cat.id, isEnabled: !cat.isEnabled })}
+                                >
+                                  <Icon name={cat.isEnabled ? 'minus square' : 'plus square'} />
+                                  {cat.isEnabled ? 'Disable' : 'Enable'}
+                                </Button>
+                                <Button size='tiny' negative onClick={() => handleDelete(cat)}>
+                                  <Icon name='trash alternate' />
+                                  Delete
+                                </Button>
+                              </Item.Description>
+                            </Item.Content>
+                          </Grid.Column>
+                        </Grid.Row>
+                      </Grid>
+                    </Item>
+                  )
+                })}
+              </Item.Group><Button icon disabled={0 > pageIndex - PAGE_SIZE}
+                onClick={() => setPageIndex(pageIndex - PAGE_SIZE)}
+                style={{ width: '5em' }}
+              >
+                <Icon name='angle left' />
+              </Button><Button icon disabled={categoryResult.data.userCategories.length <= pageIndex + PAGE_SIZE}
+                onClick={() => setPageIndex(pageIndex + PAGE_SIZE)}
+                style={{ width: '5em' }}
+              >
+                <Icon name='angle right' />
+              </Button>
+            </>
           }
         </Grid.Column>
       </Grid>
