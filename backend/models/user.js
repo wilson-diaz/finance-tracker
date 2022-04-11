21496cd50d31c66100e9bbf15c563d6b5a980a7d
Wilson Diaz <wdfiguereo@gmail.com>
2022-04-11 07:31:11 -0400

    refactor schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,19 +8,7 @@ const schema = new mongoose.Schema({
   passHash: {
     type: String,
     required: true
-  },
-  categories: [
-    { 
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Category'
-    }
-  ],
-  transactions: [
-    { 
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Transaction'
-    }
-  ]
+  }
 })
 
 module.exports = mongoose.model('User', schema)
